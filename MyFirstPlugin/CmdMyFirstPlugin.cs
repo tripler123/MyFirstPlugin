b92177dc401e083ffe6c102a45eedc607232ed60
tripler123 <rios.renzo@pucp.pe>
2025-02-02 20:59:02 -0500

    Add project files.

---
+++ MyFirstPlugin/CmdMyFirstPlugin.cs
@@ -0,0 +1,19 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.ApplicationServices;
+
+namespace MyFirstPlugin
+{
+    public class CmdMyFirstPlugin : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            TaskDialog.Show("Title", "Mensaje de prueba");
+
+
+            TaskDialog.Show("Title", "Mensaje de prueba"); // 
+
+            return Result.Succeeded;
+        }
+    }
+}
